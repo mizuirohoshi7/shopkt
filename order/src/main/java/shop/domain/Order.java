cc9acdfdaa86339e5431a8c55acda2bd784f264b
mizuirohoshi7 <142835195+mizuirohoshi7@users.noreply.github.com>
2024-08-29 13:41:51 +0900

    commit

---
+++ order/src/main/java/shop/domain/Order.java
@@ -0,0 +1,126 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import shop.OrderApplication;
+import shop.domain.OrderCanceled;
+import shop.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long customerId;
+
+    private String customerName;
+
+    private Long productId;
+
+    private Integer qty;
+
+    private String address;
+
+    private String status;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+        OrderCanceled orderCanceled = new OrderCanceled(this);
+        orderCanceled.publishAfterCommit();
+    }
+
+    @PreRemove
+    public void onPreRemove() {}
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void sendMail(InventoryIncreased inventoryIncreased) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Order order = new Order();
+        repository().save(order);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(inventoryIncreased.get???()).ifPresent(order->{
+            
+            order // do something
+            repository().save(order);
+
+
+         });
+        */
+
+    }
+
+    //>>> Clean Arch / Port Method
+    //<<< Clean Arch / Port Method
+    public static void updateStatus(DeliveryStarted deliveryStarted) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Order order = new Order();
+        repository().save(order);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(deliveryStarted.get???()).ifPresent(order->{
+            
+            order // do something
+            repository().save(order);
+
+
+         });
+        */
+
+    }
+
+    //>>> Clean Arch / Port Method
+    //<<< Clean Arch / Port Method
+    public static void updateStatus(DeliveryCanceled deliveryCanceled) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Order order = new Order();
+        repository().save(order);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(deliveryCanceled.get???()).ifPresent(order->{
+            
+            order // do something
+            repository().save(order);
+
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
---
+++ support/src/main/java/shop/domain/DeliveryCanceled.java
@@ -0,0 +1,28 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import shop.domain.*;
+import shop.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryCanceled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String address;
+    private Long productId;
+    private Integer qty;
+
+    public DeliveryCanceled(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryCanceled() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
